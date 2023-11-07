042372534fb31d594305056c651749bf7e3e89d0
Serhat Sel <selserhat01@gmail.com>
2023-11-07 15:20:38 +0300

    ejs eklendi public ve views düzenlendi

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res, next) => {
-  res.send("Index sayfası")
+//? ejs template engine
+
+app.set("view engine", "ejs");
+
+//?static files middleware
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(port, () => {
